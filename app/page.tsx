5db07c0b84ba958284edbef8931ec2cdbf4c96b2
sam-zarila <zarilatroxx99@gmail.com>
2025-01-03 01:54:19 -0800

    landin page

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import Header from "./components/header";
 import Hero from "./home/hero";
 import TrustedPartner from "./home/steps";
-import Services from "./home/services";
+// import Services from "./home/services";
 
 export default function Home(){
   return(
     <>
 <Header/> 
 <Hero/>   
-<Services/>
+
 <TrustedPartner/> 
     </>
   )
