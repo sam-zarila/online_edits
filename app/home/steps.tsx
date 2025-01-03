51628cb956f3c54d00fd2a11e2eab87734077747
sam-zarila <zarilatroxx99@gmail.com>
2025-01-03 01:14:14 -0800

    landing page

---
+++ app/home/steps.tsx
@@ -0,0 +1,26 @@
+export default function TrustedPartner() {
+    return (
+      <section className="bg-darkBg text-lightGray py-20 px-6">
+        <div className="max-w-7xl mx-auto">
+          <h2 className="text-4xl font-bold text-white mb-10 text-center">
+            Your <span className="text-neon">trusted</span> partner of cryptocurrency.
+          </h2>
+          <div className="grid md:grid-cols-3 gap-8">
+            <div className="bg-gray-800 p-12 rounded-md text-center">
+              <h3 className="text-xl font-bold text-neon">01.</h3>
+              <p>Service for Any Level of Expertise.</p>
+            </div>
+            <div className="bg-gray-800 p-12 rounded-md text-center">
+              <h3 className="text-xl font-bold text-neon">02.</h3>
+              <p>Industry best practices.</p>
+            </div>
+            <div className="bg-gray-800 p-24 rounded-md text-center">
+              <h3 className="text-xl font-bold text-neon">03.</h3>
+              <p>Protected by Insurance.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/header";
 import Hero from "./home/hero";
+import TrustedPartner from "./home/steps";
 
 export default function Home(){
   return(
     <>
 <Header/> 
-<Hero/>     
+<Hero/>    
+<TrustedPartner/> 
     </>
   )
 }
